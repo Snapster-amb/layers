7c4222a7e01ea228a0d30cdfa982def96ba27171
Snapster-amb <jcperschon@gmail.com>
2025-02-17 14:56:53 -0700

    Removed unecessary packet registration

--- shims/memory.lua
+++ shims/memory.lua
@@ -10,15 +10,4 @@ memory.GetMainJobLevel = function()
     return player.main_job_level
 end
 
-local function updatePlayerSyncLevel(id, data, modified, injected, blocked)
-    if id == 0x061 then
-        local level = data:byte(0x0d + 1)
-        if level ~= syncLevel then
-            syncLevel = level
-        end
-    end
-end
-
-windower.raw_register_event('incoming chunk', updatePlayerSyncLevel)
-
 return memory
